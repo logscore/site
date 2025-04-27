668ae849e0386028df7f3e422b9ab3660cb5e289
Logan Reeder <lsreedercontact@gmail.com>
2025-04-26 21:22:32 -0600

    Added awesome list

--- src/routes/awesome/+page.svelte
+++ src/routes/awesome/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import Awesome from '@/custom/md/Awesome.svx';
-
-	let { data }: { data: PageData } = $props();
 </script>
 
-<div class="prose mx-6 dark:prose-dark">
+<div class="prose mx-6 max-w-full dark:prose-dark">
 	<Awesome />
 </div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import Projects from '@/custom/md/Projects.svx';
-
-	let { data }: { data: PageData } = $props();
 </script>
 
-<div class="prose mx-6 dark:prose-dark">
+<div class="prose mx-6 max-w-full dark:prose-dark">
 	<Projects />
 </div>
